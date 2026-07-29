8bdc815a38ed53115c0d9e307e4a15a0f63d8ea4
IzaVieceli <126038205+IzaVieceli@users.noreply.github.com>
2026-07-29 17:18:28 -0300

    sf

--- peso.js
+++ peso.js
@@ -1,27 +1,31 @@
-function atualizarPeso(){
+function atualizarPeso() {
 
     let peso = document.getElementById("peso").value;
 
-    if(peso == ""){
+    if (peso == "") {
+
         document.getElementById("valorPeso").innerHTML = "";
         document.getElementById("continuar").disabled = true;
         return;
+
     }
 
-    document.getElementById("valorPeso").innerHTML = peso + " kg";
+    document.getElementById("valorPeso").innerHTML =
+        "Peso selecionado: <strong>" + peso + " kg</strong>";
 
     document.getElementById("continuar").disabled = false;
 
 }
 
-function salvarPeso(){
+function salvarPeso() {
 
     let personagem = JSON.parse(localStorage.getItem("personagem"));
 
     personagem.peso = document.getElementById("peso").value;
 
-    localStorage.setItem("personagem",JSON.stringify(personagem));
+    localStorage.setItem("personagem", JSON.stringify(personagem));
 
-    window.location.href="pele.html";
+    // Próxima tela
+    window.location.href = "pele.html";
 
 }
