2c3bd74908bad915a8cc12875b4e4bd2589e5198
IzaVieceli <126038205+IzaVieceli@users.noreply.github.com>
2026-07-29 17:07:23 -0300

    funcao peso

--- peso.js
+++ peso.js
@@ -1,16 +1,26 @@
-function salvarPeso(){
+function atualizarPeso(){
 
-    let opcao = document.querySelector('input[name="peso"]:checked');
+    let peso = document.getElementById("peso").value;
 
-    if(opcao == null){
-
-        alert("Escolha um biotipo.");
-
+    if(peso == ""){
+        document.getElementById("valorPeso").innerHTML = "";
+        document.getElementById("continuar").disabled = true;
         return;
-
     }
 
-    localStorage.setItem("peso",opcao.value);
+    document.getElementById("valorPeso").innerHTML = peso + " kg";
+
+    document.getElementById("continuar").disabled = false;
+
+}
+
+function salvarPeso(){
+
+    let personagem = JSON.parse(localStorage.getItem("personagem"));
+
+    personagem.peso = document.getElementById("peso").value;
+
+    localStorage.setItem("personagem",JSON.stringify(personagem));
 
     window.location.href="pele.html";
 
